06c54d95aa765f91c9a1a8d1023da048895d947c
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-24 16:03:58 -0300

    Adicionar arquivos de projeto.

---
+++ 04 - Manuel - Class Exercise 2.0/Entities/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities {
+    class Calculator {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Calculator() {
+        }
+        public Calculator(double width, double height) {
+            Width = width;
+            Height = height;
+        }
+        public double Area() {
+            return Width * Height;
+        }
+
+        public double Perimeter() {
+            return (2 * Width) + (2 * Height);
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt(Width*Width + Height*Height);
+        }
+        public override string ToString() {
+            return "Area do retângulo: " + Area() + "\nPerimetro do retângulo: " + Perimeter() + "\nDiagonal do retângulo: " +Diagonal().ToString("F2");
+        }
+    }
+}
---
+++ 04 - Manuel - Class Exercise 2.0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Entities;
+namespace _04___Manuel___Class_Exercise_2._0 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine("---------------------------");
+
+            Calculator calculator = new Calculator(width, height);
+            Console.WriteLine(calculator);
+            
+
+
+        }
+    }
+}
